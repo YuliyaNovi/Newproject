2326eada4781d104370ce4ef76f4255bc67a9b79
Student <9723903@mail.ru>
2023-07-10 14:50:03 +0300

    Стихи Некрасова и параметры в декораторе

--- main.py
+++ main.py
@@ -26,15 +26,49 @@ def poster():
 <head>
     <meta charset="UTF-8">
     <title>Постер</title>
+    <link rel="stylesheet" type="text/css" href ="{url_for('static', filename = 'css/style.css')}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
 <body>
-<h1>Постер к фильму</h1>
+<h1 class="red">Постер к фильму</h1>
 <img src="{url_for('static', filename = 'images/admiral.jpeg')}" alt ="Здесь должна была быть картинка, но не нашлась" >
 <p>И крепка, как смерть, любовь!</p>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>"""
 
+@app.route('/nekrasov/<username>')
+def nekrasov(username):
+    return f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>{username}</title>
+        <link rel="stylesheet" type="text/css" href ="{url_for('static', filename='css/style.css')}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+    <h1 class="text-center">Стихи Некрасова, {username}</h1>
+    <div class="p-3 mb-2 bg-info text-white container">
+    <div class="row"> 
+    <div class="col">
+    <img src="{url_for('static', filename='images/nekrasov.jpg')}" class="rounded-circle" alt ="Здесь должна была быть картинка, но не нашлась" >
+    </div>
+    <div class="col">
+    <p class="text-warning bg-dark text-center">Однажды, в студеную зимнюю пору,<br> Я из лесу вышел; был сильный мороз. <br>
+    Гляжу, поднимается медленно в гору<br>Лошадка, везущая хворосту воз.</p>
+    <p class="text-dark bg-success text-center">И, шествуя важно, в спокойствии чинном,<br> Лошадку ведет под уздцы мужичок<br> В больших сапогах, в полушубке овчинном,<br>
+     В больших рукавицах… а сам с ноготок!</p>
+     </div>
+     </div></div>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
