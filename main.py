50a8fee7f1bf007e2c7fd8d4c4bfba48bd90d76f
Student <9723903@mail.ru>
2023-07-10 16:05:41 +0300

    Карусель

--- main.py
+++ main.py
@@ -8,6 +8,46 @@ def index():
            '<a href="/coutdown">Отсчет</a><br> ' \
            '<a href="/slogan">Слоган </a>'
 
+@app.route('/slideshow') # карусель
+def slideshow():
+    return f"""<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Постер</title>
+        <link rel="stylesheet" type="text/css" href ="{url_for('static', filename='css/style.css')}">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+    <h1 class="red">Карусель</h1>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="{url_for('static', filename='images/1.jpg')}" class="d-block w-100" alt="Картинка 1">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='images/2.jpg')}" class="d-block w-100" alt="Картинка 2">
+    </div>
+    <div class="carousel-item">
+      <img src="{url_for('static', filename='images/3.jpg')}" class="d-block w-100" alt="Картинка 3">
+    </div>
+  </div>
+ <button class="carousel-control-prev" type="button" data-target="#carouselExampleControls" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleControls" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>"""
+
+
 @app.route('/slogan')
 def slogan():
     return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a> '
