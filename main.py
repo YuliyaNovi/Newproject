0c00b557dc04b3e2d76da2574a0f0d2e4cf049a7
Student <9723903@mail.ru>
2023-07-10 12:02:39 +0300

    Flask first step @route a href

--- main.py
+++ main.py
@@ -4,8 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello'
+    return 'Адмирал<br> ' \
+           '<a href="/coutdown">Отсчет</a><br> ' \
+           '<a href="/slogan">Слоган </a>'
+
+@app.route('/slogan')
+def slogan():
+    return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a> '
+
+@app.route('/coutdown')
+def coutdown():
+    lst = [str(x) for x in range(10, 0, -1)]
+    lst.append('Start!!!')
+    return '<br>'.join(lst)
+
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
+
+
