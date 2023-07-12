b8083844cf574980cd8930d7a1b3c77d6caa4e86
Student <9723903@mail.ru>
2023-07-12 14:54:18 +0300

    Форма логин пароль начало

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from flask import Flask, url_for, request, redirect
 from flask import render_template
 import json
 import requests
+from loginform import LoginForm
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'too short key'
-
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-#    user = "Слушатель"
+    #    user = "Слушатель"
     param = {}
     param['username'] = 'Слушатель'
     param['title'] = 'Расширяем шаблоны'
     return render_template('index.html', **param)  # * список **словарь
+
+
 #  return render_template('index.html', title='Работа с шаблонами', username=user)
 
 #  return redirect('/load_photo')  # безусловный редирект, сразу кидает пользователя на страницу с главной
@@ -22,14 +25,29 @@ def index():
 def http_404_error(error):
     return redirect('/error404')
 
+
 @app.route('/error404')
 def well():  # колодец
     return render_template('well.html')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'Success'
+
+
 @app.route('/weather_form', methods=['GET', 'POST'])
 def weather_form():
     if request.method == 'GET':
-            return render_template('weather_form.html', title="Погода")
+        return render_template('weather_form.html', title="Погода")
     elif request.method == 'POST':
         town = request.form.get('town')
         data = {}
@@ -46,23 +64,26 @@ def weather_form():
         data['icon'] = icon
         data['temp'] = temp
 
-        return render_template('weather.html', title=f'Погода в городе{town}', town=town, data=weather, icon=icon, temp=temp, vlag=vlag)
-
+        return render_template('weather.html', title=f'Погода в городе{town}', town=town, data=weather, icon=icon,
+                               temp=temp, vlag=vlag)
 
 
 @app.route('/var_test')
 def var_test():
     return render_template('var_test.html', title='Определяем переменную внутри HTML')
 
+
 @app.route('/odd_even')
 def odd_even():
     return render_template('odd_even.html', number=3)
+
 
 @app.route('/news')
 def news():
     with open('news.json', 'rt', encoding='utf-8') as f:
         news_list = json.loads(f.read())
     return render_template('news.html', title='Новости', news=news_list)
+
 
 # @app.route('/news')
 # def news():
@@ -77,7 +98,7 @@ def slogan():
 @app.route('/form_sample', methods=['GET', 'POST'])
 def form_sample():
     if request.method == 'GET':
-            return render_template('user_form.html', title="Форма")
+        return render_template('user_form.html', title="Форма")
     elif request.method == 'POST':
         f = request.files['file']
         f.save('./static/images/loaded.png')
@@ -106,8 +127,6 @@ def load_photo():
         return '<h1>Файл у вас на сервере</h1>'
 
 
-
-
 @app.route('/poster')
 def poster():
     return f"""<!DOCTYPE html>
@@ -127,7 +146,6 @@ def poster():
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>"""
-
 
 
 if __name__ == '__main__':
