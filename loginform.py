a25e0365a795858cd70acb407c94530c5d20a618
Student <9723903@mail.ru>
2023-07-12 15:23:40 +0300

    Форма логин пароль

--- loginform.py
+++ loginform.py
@@ -4,9 +4,13 @@ from wtforms import SubmitField
 from wtforms.validators import DataRequired
 
 
+# from wtforms import FileField
+# Если из формы добавлен файл, то обращаться к нему при обработке формы следует так:
+# f.form <название поля с файлом>.data
+
 class LoginForm(FlaskForm):
     username = StringField("Логин", validators=[DataRequired()])
     password = PasswordField("Пароль", validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
+    #   file = FileField('Файл')
     submit = SubmitField('Войти')
-
