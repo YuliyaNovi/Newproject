bd20e842e2fee3e8e0b3b71eac6bf7ee060945a3
Student <9723903@mail.ru>
2023-07-13 10:14:58 +0300

    ORM создание бд, создание таблицы работает Column с большой буквы

--- data/users.py
+++ data/users.py
@@ -6,9 +6,9 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
-    id = sqlalchemy.column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.column(sqlalchemy.String, index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.column(sqlalchemy.String, nullable=True)
-    create_date = sqlalchemy.column(sqlalchemy.DateTime, default=datetime.now())
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    create_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
