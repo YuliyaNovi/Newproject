f175e467574c62a299b333c65de9824eda22fa97
Student <9723903@mail.ru>
2023-07-13 16:46:45 +0300

    Почта нужно сделать

--- main.py
+++ main.py
@@ -146,26 +146,16 @@ def load_photo():
         f.save('./static/images/loaded.png')
         return '<h1>Файл у вас на сервере</h1>'
 
-
-@app.route('/poster')
-def poster():
-    return f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Постер</title>
-    <link rel="stylesheet" type="text/css" href ="{url_for('static', filename='css/style.css')}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-</head>
-<body>
-<h1 class="red">Постер к фильму</h1>
-<img src="{url_for('static', filename='images/admiral.jpeg')}" alt ="Здесь должна была быть картинка, но не нашлась" >
-<p>И крепка, как смерть, любовь!</p>
-
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-</body>
-</html>"""
+# @app.route('/mail', methods=['GET'])
+# def get_form():
+#     return render_template('mail_send.html')
+#
+# @app.route('/mail', method=['POST'])
+# def post_form():
+#     email = request.values.get('email')
+#     if send_mail(email, "Вам письмо", "Тема письма"):
+#         return f'Письмо на адрес {email} отправлено успешно!'
+#     return "Сбой при отправке"
 
 
 if __name__ == '__main__':
